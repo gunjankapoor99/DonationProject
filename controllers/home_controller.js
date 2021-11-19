b8aa056cc90a072c4921f5dfa9556cbd9849e0ff
darklegend99 <darklegend1931@gmail.com>
2021-11-19 21:03:39 +0530

    Set up header.ejs and layout.ejs

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,3 +1,5 @@
+const User = require('../models/user');
+
 module.exports.home = function(req, res){ 
    return res.end('<h1>This is our first team project</h1>');
     return res.render('home', {
--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+const expressLayouts = require('express-ejs-layouts');
+
 app.use(express.urlencoded());
 app.use(express.static('./assets'));
+
+app.use(expressLayouts);
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
