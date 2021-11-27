fe9f918e56f7dbed24ce330b8be419c393b72687
darklegend99 <darklegend1931@gmail.com>
2021-11-27 11:16:11 +0530

    Resolve passport conflicts

--- index.js
+++ index.js
@@ -12,8 +12,6 @@ const passportLocal = require('./config/passport-local-strategy');
 // const passportGoogle = require('./config/passport-google-oauth2-strategy');
 const MongoStore = require('connect-mongo')(session);
 
-const flash = require('connect-flash');
-const customMware = require('./config/middleware');
 
 app.use(express.urlencoded());
 app.use(cookieParser());
@@ -26,12 +24,6 @@ app.set('layout extractScripts', true);
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
-app.use(passport.initialize());
-app.use(passport.session());
-
-app.use(passport.setAuthenticatedUser);
-app.use(flash());
-app.use(customMware.setFlash);
 
 app.use('/', require('./routes'));
 
