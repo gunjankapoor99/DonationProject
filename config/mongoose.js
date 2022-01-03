051bb402e9392faa751a3c8460b3a32625fc61da
UTKARSH GUPTA <utkarsh.19bcon013@jecrcu.edu.in>
2022-01-03 16:47:42 +0530

    change

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://Jatin-Gupta:8V2pxFy2Lbo8OVyW@cluster0.wjzw0.mongodb.net/ug?authSource=admin&replicaSet=atlas-j4d0mo-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true');
+mongoose.connect('mongodb://localhost/donationProjectUsersDB');
 
 const db = mongoose.connection;
 
