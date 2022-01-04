30cae89dcc761a01ad5440ec7d61af74d99f50ea
darklegend99 <darklegend1931@gmail.com>
2022-01-04 23:39:46 +0530

    Create apiFeatures file

--- controllers/faq_controller.js
+++ controllers/faq_controller.js
@@ -1,91 +1,12 @@
 const db = require('../config/mongoose');
+const ApiFeatures = require("../utils/apiFeatures");
 // const Faqs = require('../models/faqs');
 
-const Faqs = [
-    {
-        id: 1,
-        query: "This is Question One",
-        answer: "This is answer One"
-    },
-    {
-        id: 2,
-        query: "This is Question Two",
-        answer: "This is answer Two"
-    },
-    {
-        id: 3,
-        query: "This is Question Three",
-        answer: "This is answer Three"
-    },
-    {
-        id: 4,
-        query: "This is Question Four",
-        answer: "This is answer Four"
-    },
-    {
-        id: 5,
-        query: "This is Question One",
-        answer: "This is answer One"
-    },
-    {
-        id: 6,
-        query: "This is Question One",
-        answer: "This is answer One"
-    },
-    {
-        id: 7,
-        query: "This is Question One",
-        answer: "This is answer One"
-    },
-    {
-        id: 8,
-        query: "This is Question One",
-        answer: "This is answer One"
-    },
-    {
-        id: 9,
-        query: "This is Question One",
-        answer: "This is answer One"
-    },
-    {
-        id: 10,
-        query: "This is Question One",
-        answer: "This is answer One"
-    },
-    {
-        id: 11,
-        query: "This is Question One",
-        answer: "This is answer One"
-    },
-    {
-        query: "This is Question One",
-        answer: "This is answer One"
-    },
-    {
-        query: "This is Question One",
-        answer: "This is answer One"
-    },
-    {
-        query: "This is Question One",
-        answer: "This is answer One"
-    },
-    {
-        query: "This is Question One",
-        answer: "This is answer One"
-    },
-    {
-        query: "This is Question One",
-        answer: "This is answer One"
-    },
-    {
-        query: "This is Question 17",
-        answer: "This is answer 17"
-    },
-    {
-        query: "This is Question 18",
-        answer: "This is answer 18"
-    }
-];
+const Faqs = require("../faqs.json");
+
+module.exports.search = function(req, res){
+    
+}
 
 module.exports.faq = function(req, res){ 
     return res.render('faq', {
@@ -93,5 +14,4 @@ module.exports.faq = function(req, res){
         message: "This is our faqs page",
         faqs_list: Faqs
     });
-
 };
