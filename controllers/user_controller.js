ee4b2b7b708737e1ac570542fba524c18c97ee7f
darklegend99 <darklegend1931@gmail.com>
2022-01-11 20:33:09 +0530

    Add avatar feature to user profile

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -19,6 +19,49 @@ module.exports.createUser = function(req, res){
     });
 };
 
+module.exports.editProfile = function(req, res){
+    User.findById(req.params.id, function(err, user){
+        console.log(user);
+        return res.render('editProfile', {
+            title: 'Edit Profile',
+            profile_user: user
+        });
+    }); 
+};
+
+module.exports.update = async function(req, res){
+
+    if(req.user.id == req.params.id){
+        try{
+            let user = await User.findById(req.params.id);
+            User.uploadedAvatar(req, res, function(err){
+                if (err) {console.log('****Multer Error: ', err)}
+                console.log(req.file);
+
+                user.name = req.body.name;
+                user.email = req.body.email;
+
+                if (req.file){
+
+                    if (user.avatar){
+                        fs.unlinkSync(path.join(__dirname, '..', user.avatar));
+                    }
+                    user.avatar = User.avatarPath + '/' + req.file.filename;
+                }
+                user.save();
+
+                return res.redirect('back');
+            });
+        }catch(err){
+            console.log('error', err);
+            return res.redirect('back');
+        }
+    }else{
+        req.flash('error', 'Unauthorized!');
+        return res.status(401).send('Unauthorized');
+    }
+}
+
 module.exports.signIn = function(req, res){
     if(req.isAuthenticated()){
         return res.redirect('/users/profile');
