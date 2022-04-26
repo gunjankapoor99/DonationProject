b5c8dc3085992dbcf554b1088cfdaa60ee3210de
nik-agarwal <67484380+nik-agarwal@users.noreply.github.com>
2022-04-26 22:10:28 +0530

    donation page updated

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,7 @@ const contactController = require('../controllers/contact_controller');
 const donateController = require('../controllers/donate_controller');
 const volunteerController = require('../controllers/volunteer_controller');
 const brandPartnersController = require('../controllers/brandPartners_controller');
+const donateProceedController = require('../controllers/donateProceed_controller');
 const faqController = require('../controllers/faq_controller');
 const termsController = require('../controllers/terms_controller');
 const fundRaiserController = require('../controllers/fundRaiser_controller');
@@ -17,6 +18,7 @@ const ngoPartnersController = require('../controllers/ngoPartners_controller');
 const db = require('../config/mongoose');
 const Users = require('../models/user');
 const Faqs = require('../models/faqs');
+const { donateProceed } = require('../controllers/donateProceed_controller');
 
 
 console.log('index router loaded!');
@@ -32,6 +34,7 @@ router.get('/donate', donateController.donate);
 router.get('/fundRaiser', fundRaiserController.fundRaiser);
 router.get('/freeFundRaiser', freeFundRaiserController.freeFundRaiser);
 router.get('/volunteer', volunteerController.volunteer);
+router.get('/donateProceed', donateProceedController.donateProceed);
 router.get('/brandPartners', brandPartnersController.brandPartners);
 router.get('/ngoPartners', ngoPartnersController.ngoPartners);
 router.use('/users', require('./users'));
